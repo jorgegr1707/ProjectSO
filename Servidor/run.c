714088c1221a13a145fa8c0b7b594d9917d2fea0
Ruth <ruthmub06@gmail.com>
2018-04-05 10:40:29 -0600

    Comments added :)

--- Servidor/run.c
+++ Servidor/run.c
@@ -23,46 +23,24 @@ int main()
     printf("\nEnter Choice 0-4? : ");
     scanf("%d", &algorithm_type);
 
-    printf("\nBurst: ");
-    scanf("%d", &burst);
+    if (algorithm_type == 4)
+    {
+    	printf("\nQuantum: ");
+    	scanf("%d", &burst);
+    }
 
     flag = 1;
     
     pthread_t clock_thread, job_scheduler, cpu_scheduler;
+	
 	pthread_create(&clock_thread, NULL, (void*)clock_action, NULL);
 	pthread_create(&job_scheduler, NULL, (void*)job_scheduler_action, NULL);
 	pthread_create(&cpu_scheduler, NULL, (void*)cpu_scheduler_action, NULL);
 
    	pthread_join(clock_thread, NULL);
-
 	pthread_join(job_scheduler, NULL);
-		
 	pthread_join(cpu_scheduler, NULL);
 
 	scanf("%d", &flag);
-	
-	printf("xxxxxxxxxxxxxxxxxxxxx\n");
 
 }
-
-
-/*
-		// Recibe datos
-		char* buffer = malloc(10);
-		int bytesRecibidos = recv(connect, buffer, 10, 0);
-		printf("Recibidos: %s\n", buffer);
-
-
-		// Duerme un rato
-		srand(time(0));
-		int r = 8 + rand() % 4;
-		printf("- - - Voy a dormir: %d\n", r);
-		sleep(r);
-		
-		// Envia datos
-		char *msg = advice[rand()%5];
-		send(connect, msg, strlen(msg), 0);
-		msg = NULL;
-		printf("- - - Le conteste a: %s\n", buffer);
-		close(connect);
-*/
--- Servidor/src/algorithms.c
+++ Servidor/src/algorithms.c
@@ -1,71 +1,85 @@
-
+// ###############################################################################
+// This function receives the necessaries parameters to insert in the ready queue 
+// depending of the algorithm selectionated
+// ###############################################################################
 void insert_ready_queue(int id, int burst, int priority, int waiting_time, int turn_around_time)
 {
 	switch (algorithm_type)
 	        {
-	            case 1:
+	            case 1: //FCFS Algorithm
 	                append(id, burst, priority, waiting_time, clock_cpu, turn_around_time);
 	                break;
-	            case 2:
+	            case 2: //SJF Algorithm
 	            	insert_by_burst(id, burst, priority, waiting_time, clock_cpu, turn_around_time);
 	                break;
-	            case 3:
+	            case 3: //HPF Algorithm
 	                insert_by_priority(id, burst, priority, waiting_time, clock_cpu, turn_around_time);
 	                break;
-	            case 4:
-	            	rr = 1;
+	            case 4: //Round Robin Algorithm
+	            	rr = 1; 
 	            	append(id, burst, priority, waiting_time, clock_cpu, turn_around_time);
 	                break;
 	        }
-
 }
 
-// ##############################################
-// Se encarga de estar constantemente buscando clientes.
-// cuando encuentra uno, lo envia a la cola ## falta implementarlo
-// ##############################################
+
+// ###############################################################################
+// This thread is looking for clients all the time and inserts the process in the 
+// ready queue when finds one
+// ###############################################################################
 void * job_scheduler_action(void * args)
 {
-	// Inicia el listener
-	int listener = begin_listener();
+	
+	int listener = begin_listener(); // Start listener
 
-	// Inicia el puerto
-	const int PORT = 7200;
+	const int PORT = 7200; // Start port
 	begin_port(listener, PORT);
 
-	// Espera por clientes. Cuando encuentra uno, lo ingresa a la cola
-	while(1)
+	if (flag)
 	{
-		// Crea la estructura del cliente que se va a recibir
-		struct sockaddr_storage client;
-		unsigned int address_size = sizeof(client);
+		while(1) //Wait for clients
+		{
+			// Create the struct of the client
+			struct sockaddr_storage client;
+			unsigned int address_size = sizeof(client);
 
-		// Verifica si algun cliente necesita el servicio
-		int connect = accept(listener, (struct sockaddr*)&client, &address_size);
-		
-		//recibe los datos del cliente
-		char* buffer = malloc(10);
-		recv(connect, buffer, 10, 0);
-		
-		char string[10];
-		strcpy(string, buffer);
-		char *splitted_buffer[10];
-		//this makes the split
-		splitted_buffer[0] = strtok(string,"-"); //burst
-		splitted_buffer[1] = strtok(NULL,"-");	//priority
-		insert_ready_queue(id, atoi(splitted_buffer[0]), atoi(splitted_buffer[1]), 0, 0);
+			// Verify if there is a client
+			int connect = accept(listener, (struct sockaddr*)&client, &address_size);
+			
+			// Receive data from the cliente in buffer, it's a string "burst-priority"
+			char* buffer = malloc(10);
+			recv(connect, buffer, 10, 0);
+			
+			// This section makes the split, converts string to int and inserts data in the ready queuee
+			char string[10];
+			strcpy(string, buffer);
+			char *splitted_buffer[10];
+			// Make the split
+			splitted_buffer[0] = strtok(string,"-"); //burst
+			splitted_buffer[1] = strtok(NULL,"-");	//priority
+			insert_ready_queue(id, atoi(splitted_buffer[0]), atoi(splitted_buffer[1]), 0, 0);
 
-		char message[50]; 
-		strcpy(message, "Proceso recibido, con ID: ");
-		char result[50];
-		itoa(connect, result, 10);
-		strcat(message, result);
-		send(connect, message, strlen(message),0); //contesta al cliente
-		id++;
+			char message[50]; 
+			strcpy(message, "Process received, with ID: ");
+			char result[50];
+			itoa(connect, result, 10);
+			strcat(message, result);
+			send(connect, message, strlen(message),0); //Send received message to client
+			id++; //Increment id for the next process
+		}
+
 	}
-	pthread_exit(0);
+	else
+	{
+		pthread_exit(0);
+	}
+	
 }
 
+
+// ###############################################################################
+// This thread executes the processes of the ready queue
+// ###############################################################################
 void * cpu_scheduler_action(void * args)
 {
 	struct dnode *temp;
@@ -77,29 +91,34 @@ void * cpu_scheduler_action(void * args)
 		{
 			if (temp != NULL)
 			{
-				
+				// Set the process waiting time 
 				temp->process->waiting_time = temp->process->waiting_time + (clock_cpu - temp->process->arrival_time);
-				printf("Tiempo de llegada: %d\n", temp->process->arrival_time);
-				printf("Timer: %d\n", clock_cpu);
-				printf("Ejecutando hilo: %d con burst: %d ha esperado: %d\n", temp->process->process_id,temp->process->burst, temp->process->waiting_time);
+				printf("Executing process: %d burst: %d has waited: %d\n", temp->process->process_id,temp->process->burst, temp->process->waiting_time);
 
 				if(rr == 0 || (rr == 1 && temp->process->burst <= burst))
 				{
+					// Simulate the time in the CPU
 					sleep(temp->process->burst);
+					//Set the process turn around time
 					temp->process->turn_around_time = temp->process->waiting_time + temp->process->burst;
+					// Insert process in the finished processes queue
 					append_end(temp->process->process_id, 0, temp->process->priority, temp->process->waiting_time, temp->process->turn_around_time);
-					printf("El proceso: %d ha terminado su ejecucion\n", temp->process->process_id);
+					printf("Process: %d has finished.\n", temp->process->process_id);
 				}
 				else
 				{
+					// Simulate the time in the CPU 
 					sleep(burst);
+					//Set the process burst
 					temp->process->burst = temp->process->burst - burst;
+					//Set the process turn around time
 					temp->process->turn_around_time = temp->process->turn_around_time + temp->process->waiting_time + burst;
+					// Insert in the ready queue again
 					insert_ready_queue(temp->process->process_id, temp->process->burst, temp->process->priority, temp->process->waiting_time, temp->process->turn_around_time);
 				}
 
 			}
-			
+			// Look for the next process
 			temp = remove_first();
 		}
 
@@ -112,11 +131,15 @@ void * cpu_scheduler_action(void * args)
 	
 }
 
+
+// ###############################################################################
+// This thread simulates the CPU clock
+// ###############################################################################
 void * clock_action(void * args)
 {
 	while(flag)
 	{
-		clock_cpu++;
+		clock_cpu++; //This variable keeps the actual time
 		sleep(1);
 	}
 }
